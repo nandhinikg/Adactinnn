e27e5dc73d5f788acadfa0717e97d3a34961abff
Win-UEFI <Win-UEFI@192.168.1.40>
2023-06-04 22:26:20 +0400

    intialization

---
+++ Project/src/org/prgm/Baseclass.java
@@ -0,0 +1,16 @@
+package org.prgm;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Baseclass{
+	WebDriver driver;
+	public void getdriver() {
+		driver=new ChromeDriver();
+		
+	}
+	
+
+	
+
+}
