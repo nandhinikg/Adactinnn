650361022775180e795196cbbbe607227e885205
Win-UEFI <Win-UEFI@192.168.1.40>
2023-06-05 20:48:22 +0400

    intial commit

---
+++ testng/src/test/java/Framework/testng/Sample.java
@@ -0,0 +1,15 @@
+package Framework.testng;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+    @Test
+	private void testcase1() {
+	System.out.println("amazon");
+
+	}
+    @Test
+    private void testcase2() {
+System.out.println("flipkart");
+	}
+}
